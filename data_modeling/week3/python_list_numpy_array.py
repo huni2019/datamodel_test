de039b86c54d79d0c17218a688ab87d205ed1af7
junyoung <huni201917@gmail.com>
2025-03-22 10:16:34 +0900

    0322 second commit

---
+++ data_modeling/week3/python_list_numpy_array.py
@@ -0,0 +1,14 @@
+store_a = [20,10,30]
+store_b = [70, 90, 70]
+
+list_sum = store_a + store_b
+print(list_sum)
+
+
+import numpy as np
+
+np_store_a = np.array(store_a)
+np_store_b = np.array(store_b)
+
+array_sum = np_store_a + np_store_b
+print(array_sum)
